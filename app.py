af1a53477c691649e8e86ea22aec9f1bf5791c65
unknown <amnakanwal1919@gmail.com>
2025-06-28 23:33:54 +0500

    Include best.pt in repo

--- app.py
+++ app.py
@@ -43,5 +43,6 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-if __na
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=False)
